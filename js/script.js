7a6b50ba498047e36e7e0edda2bb8af0a99d5b43
Joselyne97 <joselynejojo740@gmail.com>
2019-08-02 11:59:04 +0200

    I modified the selection areas

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@ $(document).ready(function() {
     $("#add-order").click(function() {
       $("#new-order").append('<div class="new-address">' + '<label for="sel">Select the Crust</label>' +     '<select class="form-control" id="sel">' + 
                '<option selected>choose...</option>' +
-                '<option>Crispy: 1,500rwf</option>' +
-                '<option>Stuffed: 2,000rwf</option>' +
-                '<option> Gluten-free: 2,500rwf</option>' +
+                '<option>Crispy     @1,500rwf</option>' +
+                '<option>Stuffed     @2,000rwf</option>' +
+                '<option> Gluten-free     @2,500rwf</option>' +
               '</select><br><br>' +
   '<label for="sel">Select the pizza topping(s)</label>' +
 
       '<select class="form-control" id="sel">' +
                '<option selected>choose...</option>'+
-                '<option>Chicken</option>' +
-                '<option>Extra-cheese</option>' +
-                '<option>Mushrooms&olive</option>' +
-                '<option>Onions</option>' +
-                '<option>Peperoni</option>' +
-                '<option>Vegetables</option>' +
+                '<option>Chicken     @3,000rwf</option>' +
+                '<option>Extra-cheese     @2,000rwf</option>' +
+                '<option>Mushrooms&olive     @1,000rwf</option>' +
+                '<option>Onions     @1,000rwf</option>' +
+                '<option>Peperoni     @2,000rwf</option>' +
+                '<option>Vegetables     @2,000rwf</option>' +
               '</select>' + '<br>' + '<br>' +
           
       '<label for="sel">Select the pizza size</label>' +
 
       '<select class="form-control" id="sel">' +
                '<option selected>choose...</option>' +
-                '<option>Large</option>' +
-                '<option>Medium</option>' +
-                '<option>Small</option>' +
+                '<option>Large  @5,000rwf</option>' +
+                '<option>Medium  @3,000rwf</option>' +
+                '<option>Small  @1,000rwf</option>' +
               '</select>' + '<br>' + '<br>' +
           
               '<label for="sel">Select the number of pizza you want</label>' +
